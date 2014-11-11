7baf220b82ac15fc4d55f9c4a6a8f61203d24080
yoshiokamasashi <yoshiokamasashi@yoshiokaseiji-no-MacBook-Pro.local>
2014-11-11 09:23:26 +0900

    Initial Commit

---
+++ ninzya/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ninzya
+//
+//  Created by 吉岡誠史 on 2014/11/11.
+//  Copyright (c) 2014年 xxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
